421e6ea308e0f6e7fca2110acd7e2f336844acf8
Nikita Schwarz <nkt.schwarz@gmail.com>
2024-10-13 12:03:59 +0300

    new gulp-pug

---
+++ gulpfile.js
@@ -0,0 +1,37 @@
+import gulp from 'gulp';
+import { paths } from './gulp/config/paths.js';
+
+// Импорт задач
+import { reset } from './gulp/tasks/reset.js';
+import { favicon } from './gulp/tasks/favicon.js';
+import { createSvgSprite } from './gulp/tasks/createSvgSprite.js';
+import { pug } from './gulp/tasks/pug.js';
+import { scss } from './gulp/tasks/scss.js'
+import { js } from './gulp/tasks/js.js';
+import { images } from './gulp/tasks/images.js';
+import { server } from './gulp/tasks/server.js';
+
+
+global.app = {
+    isBuild: process.argv.includes('--build'),
+    isDev: !process.argv.includes('--build'),
+    gulp
+};
+
+function watcher() {
+    gulp.watch(paths.pages.watch, pug);
+    gulp.watch(paths.scss.watch, scss);
+    gulp.watch(paths.images.watch, images);
+    gulp.watch(paths.js.watch, js);
+    gulp.watch(paths.sprite.watch, createSvgSprite);
+    gulp.watch(paths.favicon.watch, favicon);
+}
+
+const mainTasks = gulp.parallel(favicon, createSvgSprite, pug, scss, js, images);
+
+const dev = gulp.series(reset, mainTasks, gulp.parallel(server, watcher))
+const build = gulp.series(reset, mainTasks)
+
+export default dev;
+
+export { build }
